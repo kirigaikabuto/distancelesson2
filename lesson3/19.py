ca62f410bc8f3a7c94b545771f90992478a8303f
Your Name <tleugazy98.com>
2020-04-15 19:56:08 +0600

    asdsad

--- lesson3/19.py
+++ lesson3/19.py
@@ -1,6 +1,9 @@
 [-1,-2,-3,123,113,-54]
 pos=[]
 neg=[]
-все положительные числа в массив pos 
-негативные все в neg
-найти суммву всех чисел для массиов pos neg
+possum=0
+negsum=0
+for i in pos:
+    possum = possum+i
+for i in neg:
+    negsum = negsum+i
