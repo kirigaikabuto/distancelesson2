6317e469708a1480961ff5cbdec5b531a8277195
Your Name <tleugazy98.com>
2020-04-29 19:32:00 +0600

    sdsds

--- lesson8/project_order/backup.py
+++ lesson8/project_order/backup.py
@@ -1,3 +1,4 @@
+import json
 users=[]
 user1={
     "user_id":1,
@@ -19,3 +20,13 @@ order1={
     "id_user":1,
     "id_product":1
 }
+orders.append(order1)
+files=["users.json","products.json","orders.json"]
+data=[users,products,orders]
+n = len(files)
+for i in range(n):
+    file_name=files[i]
+    file = open(file_name,"w")
+    json_array = json.dumps(data[i],indent=4)
+    file.write(json_array)
+    file.close()
